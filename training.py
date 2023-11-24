b6837328f162d630a7e1e67985a8bec723fd5e3d
Yves-Simon Zeulner <ysz@web.de>
2023-11-24 11:39:03 +0100

    training parameterized

--- training.py
+++ training.py
@@ -17,170 +17,211 @@ from agent import DeepQLearningAgent, BreadthFirstSearchAgent
 import json
 import sys
 
-# some global variables
-version = sys.argv[1] if len(sys.argv) > 1 else "v17.1"
 
-# get training configurations
-with open("model_config/{:s}.json".format(version), "r") as f:
-    m = json.loads(f.read())
-    board_size = m["board_size"]
-    frames = m["frames"]  # keep frames >= 2
-    max_time_limit = m["max_time_limit"]
-    n_actions = m["n_actions"]
-    obstacles = bool(m["obstacles"])
-    buffer_size = m["buffer_size"]
-
-# define no of episodes, logging frequency
-episodes = 3 * (10**5)
-# episodes = 10000
-log_frequency = 500
-games_eval = 8
-
-# setup the agent
-agent = DeepQLearningAgent(
-    board_size=board_size,
-    frames=frames,
-    n_actions=n_actions,
-    buffer_size=buffer_size,
-    version=version,
+def train(
+    version="v17.1",
+    episodes=3 * (10**5),
+    log_frequency=500,
+    games_eval=8,
     gamma=0.98,
-)
-agent.print_models()
-agent.load_model("models/v17.1", iteration=300000)
+    load_buffer=False,
+    update_buffer=True,
+    load_pretrained=False,
+    pretrained_iteration=300000,
+    buffer_iteration=1,
+    epsilon_start=1.0,
+    epsilon_end=0.1,
+    decay=0.989,
+    log_label=None,
+):
+    # get training configurations
+    with open("model_config/{:s}.json".format(version), "r") as f:
+        m = json.loads(f.read())
+        board_size = m["board_size"]
+        frames = m["frames"]  # keep frames >= 2
+        max_time_limit = m["max_time_limit"]
+        n_actions = m["n_actions"]
+        obstacles = bool(m["obstacles"])
+        buffer_size = m["buffer_size"]
 
-# setup the epsilon range and decay rate for epsilon
-# define rewrad type and update frequency, see utils for more details
-epsilon, epsilon_end = 1, 0.01
-reward_type = "current"
-sample_actions = False
-n_games_training = 8 * 16
-decay = 0.989
-# buffer_path = "models/v17.1"
-buffer_path = None
+    # setup the agent
+    agent = DeepQLearningAgent(
+        board_size=board_size,
+        frames=frames,
+        n_actions=n_actions,
+        buffer_size=buffer_size,
+        version=version,
+        gamma=gamma,
+    )
+    agent.print_models()
 
+    if load_pretrained:
+        agent.load_model("models/" + version, iteration=pretrained_iteration)
 
-# use only for DeepQLearningAgent
-# play some games initially to fill the buffer
-# or load from an existing buffer (supervised)
+    # setup the epsilon range and decay rate for epsilon
+    # define rewrad type and update frequency, see utils for more details
+    epsilon = epsilon_start
+    reward_type = "current"
+    sample_actions = False
+    n_games_training = 8 * 16
+    # buffer_path = "models/v17.1"
 
-# setup the environment
-games = 512
-env = SnakeNumpy(
-    board_size=board_size,
-    frames=frames,
-    max_time_limit=max_time_limit,
-    games=games,
-    frame_mode=True,
-    obstacles=obstacles,
-    version=version,
-)
-if buffer_path:
-    agent.load_buffer(buffer_path, iteration=1)
-else:
-    ct = time.time()
-    _ = play_game2(
-        env,
-        agent,
-        n_actions,
-        n_games=games,
-        record=True,
-        epsilon=epsilon,
-        verbose=True,
-        reset_seed=False,
+    # use only for DeepQLearningAgent
+    # play some games initially to fill the buffer
+    # or load from an existing buffer (supervised)
+
+    # setup the environment
+    games = 512
+    env = SnakeNumpy(
+        board_size=board_size,
+        frames=frames,
+        max_time_limit=max_time_limit,
+        games=games,
         frame_mode=True,
-        total_frames=games * 64 * 2,
+        obstacles=obstacles,
+        version=version,
     )
-    print("Playing {:d} frames took {:.2f}s".format(games * 64, time.time() - ct))
+    if load_buffer:
+        agent.load_buffer("models/" + version, iteration=buffer_iteration)
+    else:
+        ct = time.time()
+        if update_buffer:
+            _ = play_game2(
+                env,
+                agent,
+                n_actions,
+                n_games=games,
+                record=True,
+                epsilon=epsilon,
+                verbose=True,
+                reset_seed=False,
+                frame_mode=True,
+                total_frames=games * 64 * 2,
+            )
+            print(
+                "Playing {:d} frames took {:.2f}s".format(
+                    games * 64 * 2, time.time() - ct
+                )
+            )
 
-
-env = SnakeNumpy(
-    board_size=board_size,
-    frames=frames,
-    max_time_limit=max_time_limit,
-    games=n_games_training,
-    frame_mode=True,
-    obstacles=obstacles,
-    version=version,
-)
-env2 = SnakeNumpy(
-    board_size=board_size,
-    frames=frames,
-    max_time_limit=max_time_limit,
-    games=games_eval,
-    frame_mode=True,
-    obstacles=obstacles,
-    version=version,
-)
-
-# training loop
-model_logs = {
-    "iteration": [],
-    "reward_mean": [],
-    "length_mean": [],
-    "games": [],
-    "loss": [],
-    "epsilon": [],
-}
-
-
-def generator():
-    while True:
-        yield
-
-
-# index = 0
-# for _ in tqdm(generator()):
-for index in tqdm(range(episodes)):
-    index += 1
-    # make small changes to the buffer and slowly train
-    _, _, _ = play_game2(
-        env,
-        agent,
-        n_actions,
-        epsilon=epsilon,
-        n_games=n_games_training,
-        record=True,
-        sample_actions=sample_actions,
-        reward_type=reward_type,
+    env = SnakeNumpy(
+        board_size=board_size,
+        frames=frames,
+        max_time_limit=max_time_limit,
+        games=n_games_training,
         frame_mode=True,
-        total_frames=n_games_training,
-        stateful=True,
+        obstacles=obstacles,
+        version=version,
     )
-    loss = agent.train_agent(
-        batch_size=64, num_games=n_games_training, reward_clip=True
+    env2 = SnakeNumpy(
+        board_size=board_size,
+        frames=frames,
+        max_time_limit=max_time_limit,
+        games=games_eval,
+        frame_mode=True,
+        obstacles=obstacles,
+        version=version,
     )
 
-    # check performance every once in a while
-    if (index + 1) % log_frequency == 0:
-        # keep track of agent rewards_history
-        current_rewards, current_lengths, current_games = play_game2(
-            env2,
+    # training loop
+    model_logs = {
+        "iteration": [],
+        "reward_mean": [],
+        "length_mean": [],
+        "games": [],
+        "loss": [],
+        "epsilon": [],
+    }
+
+    def generator():
+        while True:
+            yield
+
+    best_reward = float("-inf")
+    index = 0
+    for _ in tqdm(range(episodes) if (episodes >= 0) else generator()):
+        index += 1
+
+        # make small changes to the buffer and slowly train
+        _, _, _ = play_game2(
+            env,
             agent,
             n_actions,
-            n_games=games_eval,
-            epsilon=-1,
-            record=False,
-            sample_actions=False,
+            epsilon=epsilon,
+            n_games=n_games_training,
+            record=True,
+            sample_actions=sample_actions,
+            reward_type=reward_type,
             frame_mode=True,
-            total_frames=-1,
-            total_games=games_eval,
+            total_frames=n_games_training,
+            stateful=True,
+        )
+        loss = agent.train_agent(
+            batch_size=64, num_games=n_games_training, reward_clip=True
         )
 
-        model_logs["iteration"].append(index + 1)
-        model_logs["reward_mean"].append(round(int(current_rewards) / current_games, 2))
-        # model_logs['reward_dev'].append(round(np.std(current_rewards), 2))
-        model_logs["length_mean"].append(round(int(current_lengths) / current_games, 2))
-        model_logs["games"].append(current_games)
-        model_logs["loss"].append(loss)
-        model_logs["epsilon"].append(epsilon)
-        pd.DataFrame(model_logs)[
-            ["iteration", "reward_mean", "length_mean", "games", "loss", "epsilon"]
-        ].to_csv("model_logs/{:s}.csv".format(version), index=False)
+        # check performance every once in a while
+        if (index + 1) % log_frequency == 0:
+            # keep track of agent rewards_history
+            current_rewards, current_lengths, current_games = play_game2(
+                env2,
+                agent,
+                n_actions,
+                n_games=games_eval,
+                epsilon=-1,
+                record=False,
+                sample_actions=False,
+                frame_mode=True,
+                total_frames=-1,
+                total_games=games_eval,
+            )
 
-    # copy weights to target network and save models
-    if (index + 1) % log_frequency == 0:
-        agent.update_target_net()
-        agent.save_model(file_path="models/{:s}".format(version), iteration=(index + 1))
-        # keep some epsilon alive for training
-        # if index >= 400 * log_frequency:
-        epsilon = max(epsilon * decay, epsilon_end)
+            model_logs["iteration"].append(index + 1)
+            reward_mean = round(int(current_rewards) / current_games, 2)
+            model_logs["reward_mean"].append(reward_mean)
+            if reward_mean - best_reward > 5:
+                best_reward = reward_mean
+                agent.save_model(
+                    file_path="models/{:s}".format(version), iteration=(index + 1)
+                )
+                agent.save_buffer(
+                    file_path="models/{:s}".format(version), iteration=(index + 1)
+                )
+                print(f"new highscore at {reward_mean}")
+            model_logs["length_mean"].append(
+                round(int(current_lengths) / current_games, 2)
+            )
+            model_logs["games"].append(current_games)
+            model_logs["loss"].append(loss)
+            model_logs["epsilon"].append(epsilon)
+            pd.DataFrame(model_logs)[
+                ["iteration", "reward_mean", "length_mean", "games", "loss", "epsilon"]
+            ].to_csv(
+                "model_logs/{:s}{:s}.csv".format(
+                    version, f"_{log_label}" if log_label else ""
+                ),
+                index=False,
+            )
+
+            agent.update_target_net()
+            # keep some epsilon alive for training
+            # if index >= 400 * log_frequency:
+            epsilon = max(epsilon * decay, epsilon_end)
+
+
+train(
+    version="v17.1",
+    episodes=3 * (10**5),
+    log_frequency=500,
+    games_eval=8,
+    gamma=0.98,
+    load_buffer=False,
+    update_buffer=True,
+    load_pretrained=False,
+    pretrained_iteration=300000,
+    buffer_iteration=1,
+    epsilon_start=1.0,
+    epsilon_end=0.1,
+    decay=0.989,
+    log_label="test",
+)
